3b1bb00a418e28acbaf1082030abd70b88938de0
Wise <oddwiseteam@gmail.com>
2023-02-15 10:54:28 +0200

    ver_0.

--- Scripts/Jscripts.js
+++ Scripts/Jscripts.js
@@ -1,9 +1,10 @@
 "use strict"
-const numderOfMovies = prompt("Сколько фильмов вы уже посмотрели?","");
-const personalMovieDB {
-    count: numberOdMovies,
+const numberOfMovies = prompt("Сколько фильмов вы уже посмотрели?"," ");
+const personalMovieDB = {
+    count: numberOfMovies,
     movies: {},
     actors: {},
     genres: [],
     privat: false
-}
+};
+alert(personalMovieDB.count);
