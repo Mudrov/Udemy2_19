c14ecbac0b83fde4b0d6c5aeede413b9daca2196
Wise <oddwiseteam@gmail.com>
2023-02-15 10:41:07 +0200

    Ver_0.1

--- Scripts/Jscripts.js
+++ Scripts/Jscripts.js
@@ -1,1 +1,9 @@
+"use strict"
 const numderOfMovies = prompt("Сколько фильмов вы уже посмотрели?","");
+const personalMovieDB {
+    count: numberOdMovies,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
