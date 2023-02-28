29cc1a71f67a5eb11f3da17af6934cf89a3012a1
Wise <oddwiseteam@gmail.com>
2023-02-28 14:53:24 +0200

    0.9

--- Scripts/Jscripts.js
+++ Scripts/Jscripts.js
@@ -1,5 +1,16 @@
 "use strict"
 const numberOfMovies = +prompt("Сколько фильмов вы уже посмотрели?","");
+if (numberOfMovies <=10){
+    alert ('Нормальный человек, фильмы для овощей');
+    
+} else if (numberOfMovies >= 10 && numberOfMovies <= 30){
+alert ('Еще не все потеряно! Время остановиться!');
+} else if(numberOfMovies >30) {
+    alert ('Вась не спасти, вы амеба');
+} else {
+    alert ('Error');
+}
+
 const personalMovieDB = {
     count: numberOfMovies,
     movies: {
@@ -9,8 +20,15 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Последний фильм?" , ""), b = prompt("Ваша оценка" , "");
-
-personalMovieDB.movies[a] = b;
+for (let i = 1; i <= numberOfMovies; i++){
+    let a = prompt("Название фильма" , ""), b = +prompt("Ваша оценка" , "");
+        if (a != '' && b != '' && a!= null && b != null && a.length < 50 && b <= 10){
+            console.log('OK');
+            personalMovieDB.movies[a] = b;
+        } else {
+            console.log('error');
+            i--;
+        }
+}
 
 console.log(personalMovieDB);
